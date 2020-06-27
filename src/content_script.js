c4d9af4e53b4e0b72e5648b6a5bda18318e55aab
Andre Rother <andre@andrerother.de>
2020-06-27 16:41:38 +0200

    Minor changes made

--- src/content_script.js
+++ src/content_script.js
@@ -1,20 +1,22 @@
 const zettString = "ze.tt"
 const zettMainSectionClass = "cp-area--headed-zett"
-const zettSmallTeaserClass = "zon-teaser-standard__combined-link"
-
+const zonTeaserStandardClass = "zon-teaser-standard__combined-link"
 
 // Find ze.tt main-section on home page
-let zettMainSectionSelection = document.getElementsByClassName(zettMainSectionClass)
+const zettMainSectionSelection = document.getElementsByClassName(zettMainSectionClass)
 if(zettMainSectionSelection.length > 0){
     for(let i = 0; i < zettMainSectionSelection.length; i++){
         zettMainSectionSelection[i].setAttribute("style", "display:none")
     }
 }
 
-let zettSmallTeaserSelection = document.getElementsByClassName(zettSmallTeaserClass)
-for(let i = 0; i < zettSmallTeaserSelection.length; i++){
-    const currentZettTeaser = zettSmallTeaserSelection[i]
-    if(currentZettTeaser.href.match(zettString)){
-        currentZettTeaser.parentNode.setAttribute("style", "display:none")
+// Find ze.tt teaser fragments on home page 
+const zettSmallTeaserSelection = document.getElementsByClassName(zonTeaserStandardClass)
+if(zettSmallTeaserSelection.length > 0){
+    for(let i = 0; i < zettSmallTeaserSelection.length; i++){
+        const currentZettTeaser = zettSmallTeaserSelection[i]
+        if(currentZettTeaser.href.match(zettString)){
+            currentZettTeaser.parentNode.setAttribute("style", "display:none")
+        }
     }
 }
